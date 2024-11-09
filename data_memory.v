f64f7d8c95bd77f73210434ed4242d5d7530768c
Jolx77 <joaquin.otalora@mi.unc.edu.ar>
2024-11-09 18:19:30 -0300

    Debug out

--- data_memory.v
+++ data_memory.v
@@ -9,10 +9,14 @@ module data_memory #(
     input wire re,  // Señal de habilitación de lectura
     input wire [ADDR_WIDTH-1:0] addr,
     input wire [DATA_WIDTH-1:0] write_data,
-    output reg [DATA_WIDTH-1:0] read_data
+    output wire [DATA_WIDTH-1:0] read_data,
+    input wire [ADDR_WIDTH-1:0] debug_addr, // Dirección de depuración
+    output wire [DATA_WIDTH-1:0] debug_data  // Datos de depuración
 );
 
     reg [7:0] mem [0:MEM_SIZE-1]; // Memoria en bytes
+    reg [DATA_WIDTH-1:0] read_data_reg;
+    reg [DATA_WIDTH-1:0] debug_data_reg;
     integer i;
 
     always @(posedge clk or posedge rst) begin
@@ -21,10 +25,10 @@ module data_memory #(
             for (i = 0; i < MEM_SIZE; i = i + 1) begin
                 mem[i] <= 8'b0;
             end
-            read_data <= 32'b0;
+            read_data_reg <= 32'b0;
         end else if (re) begin
             // Leer 32 bits (4 bytes) desde la memoria
-            read_data <= {mem[addr], mem[addr+1], mem[addr+2], mem[addr+3]};
+            read_data_reg <= {mem[addr], mem[addr+1], mem[addr+2], mem[addr+3]};
         end
     end
 
@@ -37,5 +41,13 @@ module data_memory #(
             mem[addr+3] <= write_data[7:0];
         end
     end
+
+    always @(posedge clk) begin
+        // Leer 32 bits (4 bytes) desde la memoria para depuración
+        debug_data_reg <= {mem[debug_addr], mem[debug_addr+1], mem[debug_addr+2], mem[debug_addr+3]};
+    end
+
+    assign read_data = read_data_reg;
+    assign debug_data = debug_data_reg;
 
 endmodule

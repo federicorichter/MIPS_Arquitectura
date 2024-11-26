ea50ed739250eba97cc41e9598588060425b480e
Federico Richter <federicorichter123@gmail.com>
2024-11-26 16:05:31 -0300

    Hazard unit
    Signed-off-by: Federico Richter <federicorichter123@gmail.com>

--- hazard_detection.v
+++ hazard_detection.v
@@ -7,13 +7,14 @@ module hazard_detection #(
     input [SIZE_REG_DIR-1:0] i_rt_if_id,
     input [SIZE_REG_DIR-1:0] i_rt_id_ex,
     input i_mem_read_id_ex,
+    input i_branch,
     input i_jump_brch,
     output  o_hazard,
     output  o_flush
 );
 
     assign o_hazard = (i_mem_read_id_ex && ((i_rs_if_id == i_rt_id_ex) || (i_rt_if_id == i_rt_id_ex))) ? 1 : 0;  
-    assign o_flush = i_jump_brch ? 1 : 0;
+    assign o_flush = (i_jump_brch || i_branch)? 1 : 0;
 
     //assign o_flush = 
 
